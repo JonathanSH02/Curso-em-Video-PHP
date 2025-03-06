c6c6df0fa08af7e0e958d39c11738f894237c84b
jow <jowhayasshi@gmail.com>
2025-03-06 16:02:06 -0300

    Video em aula

---
+++ ex02/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <H1>Exemplo de PHP</H1> 
+    <?php 
+    date_default_timezone_set("America/Sao_Paulo");
+        echo "Hoje é dia " .date("d/M/Y");
+        ECHO " e a hora atual é ". date("G:i:s");
+    ?>
+</body>
+</html>
