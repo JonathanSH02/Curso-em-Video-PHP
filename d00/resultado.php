68908dd0cc2bdf70ee7468e26b986f3c86e15c94
jow <jowhayasshi@gmail.com>
2025-03-10 15:33:47 -0300

    desafio1

---
+++ d00/resultado.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado Final</h1>
+    </header>
+    <main>
+        <p>
+        <?php 
+            $n = $_GET["numero"];
+            $a = $n - 1;
+            $s = $n + 1;
+            echo "O número escolhido foi <strong>$n</strong>";
+            echo "<br>O seu antecessor é $a";
+            echo "<br>O seu sucessor é $s";
+        ?>
+        </p>
+        <button onclick="javascript:window.location.href='index.html'">Voltar</button>
+    </main>
+</body>
+</html>
